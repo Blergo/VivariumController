31d1a0a49f2820fad8ae46ae060c9045af28eb0c
Blergo <beardedowen@gmail.com>
2021-12-23 11:51:19 +0000

    Moved RTC and WiFi code to tasks

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ TFT_eSPI tft = TFT_eSPI();
 
 RTC_DS1307 rtc;
 
+TaskHandle_t TaskHandle_1;
+TaskHandle_t TaskHandle_2;
+TaskHandle_t TaskHandle_3;
+
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 0;
 const int   daylightOffset_sec = 3600;
@@ -21,24 +25,16 @@ const char* password = "K1net1CK1net1C";
 struct tm timeinfo;
 
 void TFTUpdate(void * parameter);
-
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-}
+void UpdateRTC(void * parameter);
+void initWiFi(void * parameter);
 
 void setup() {
   Serial.begin(9600);
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-  initWiFi();
+
+  xTaskCreate(initWiFi, "Initialize WiFi", 2000, NULL, 5, &TaskHandle_3);
 
   if(!rtc.begin()) {
       Serial.println("Couldn't find RTC!");
@@ -48,23 +44,39 @@ void setup() {
 
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  getLocalTime(&timeinfo);
-  //rtc.adjust(DateTime(timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec));
+  xTaskCreate(UpdateRTC, "Update RTC", 2000, NULL, 4, &TaskHandle_2);
+  xTaskCreate(TFTUpdate, "TFT Update", 2000, NULL, 1, &TaskHandle_1);
 
-  xTaskCreate(TFTUpdate, "TFT Update", 1000, NULL, 1, NULL);
 }
 
-void TFTUpdate(void * parameter){
+void initWiFi(void * parameter) {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  vTaskDelete(NULL);
+}
+
+void TFTUpdate(void * parameter) {
   for(;;){
     DateTime now = rtc.now();
     char buf1[] = "DD/MM/YY-hh:mm:ss";
     Serial.println(now.toString(buf1));
     delay(1000);
   }
+}
+
+void UpdateRTC(void * parameter) {
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  rtc.adjust(DateTime(timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec));
+  vTaskDelete(NULL);
 }
 
 void loop() {
